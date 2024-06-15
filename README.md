826ff6d40e005013340452cb4ac50b3fb90b749d
Chi-naza <ugwuokechinazajames@gmail.com>
2024-06-15 14:50:34 +0100

    Started Project

---
+++ README.md
@@ -0,0 +1,4 @@
+# realtime_camera_app
+
+This app is still in progress . . .
+
---
+++ android/app/src/main/kotlin/com/example/realtime_camera_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.realtime_camera_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
